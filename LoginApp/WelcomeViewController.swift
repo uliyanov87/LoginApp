c0b724558dcf02768ea5c302327ee52aff8e1d5f
Ulyanov Nikita <uliyanov87@icloud.com>
2022-10-02 00:23:24 +0300

    Assist controllers

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Nikita Uliyanov on 02.10.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
