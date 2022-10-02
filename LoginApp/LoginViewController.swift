36a30e03c0cbc9d48cb0a158c94d35088595cd37
Ulyanov Nikita <uliyanov87@icloud.com>
2022-10-02 04:50:10 +0300

    Done without gradient

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,33 +9,45 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: - IB Outlets
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
-        welcomeVC.userName = userNameTF.text
-    }
-    
+    // MARK: - Life Cycles Methods
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
     
-    @IBAction func forgotNameButtonDidTapped() {
-    }
-    @IBAction func forgotPasswordButtonDidTapped() {
-    }
-    @IBAction func logInButtonDidTapped() {
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if userNameTF.text == "User", passwordTF.text == "123" {
+            guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+            welcomeVC.userName = userNameTF.text
+        } else {
+            showReminder(withTitle: "Ooops", andMessage: "Wrong user name or password")
+        }
     }
     
+    // MARK: - IB Actions
+    @IBAction func forgotNameButtonDidTapped() {
+        showReminder(withTitle: "Ooops!!!", andMessage: "Your name is User😘")
+    }
+    @IBAction func forgotPasswordButtonDidTapped() {
+        showReminder(withTitle: "Ooops!!!", andMessage: "Your password is 123😘")
+    }
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         userNameTF.text = ""
         passwordTF.text = ""
+    }
+}
+
+// MARK: - Extensions
+extension LoginViewController {
+    private func showReminder(withTitle title: String, andMessage message: String) {
+        let reminder = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        reminder.addAction(okAction)
+        present(reminder, animated: true)
     }
 }
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 final class WelcomeViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
     @IBOutlet var welcomeLabel: UILabel!
     
+    // MARK: - Public Properties
     var userName: String!
     
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         welcomeLabel.text = "Welcome, \(userName ?? "")"
